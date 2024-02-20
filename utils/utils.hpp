dc449117256240031652283c4b7724a27020c310
Zakaria El bouzkri <elbouzkri9@gmail.com>
2024-02-20 11:18:24 +0100

    add toString function to convert any data to string

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: zel-bouz <zel-bouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 09:53:10 by zel-bouz          #+#    #+#             */
-/*   Updated: 2024/02/13 09:55:22 by zel-bouz         ###   ########.fr       */
+/*   Updated: 2024/02/20 11:12:41 by zel-bouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,21 @@
 #include <stdarg.h>
 #include <exception>
 
+
 template<typename T>
 char * str(const T& data) {
     std::stringstream   ss;
     ss << std::boolalpha;
     ss << data;
     return strdup(ss.str().c_str());
+}
+
+template<typename T>
+std::string toString(const T& data) {
+    std::stringstream   ss;
+    ss << std::boolalpha;
+    ss << data;
+    return ss.str();
 }
 
 
