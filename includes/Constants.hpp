9768bd04ea0606b4125f1ad4da673d1f57dabea1
Zakaria El Bouzkri <zel-bouz@e3r11p4.1337.ma>
2024-02-21 21:14:41 +0100

    create Constants for global macros

---
+++ includes/Constants.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Constants.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zel-bouz <zel-bouz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 20:46:17 by zel-bouz          #+#    #+#             */
+/*   Updated: 2024/02/21 21:04:48 by zel-bouz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#define CONF_PATH "./Configuration/servIO.conf"
+#define DEFAULT_ROOT "/tmp"
+
+#define DEFAULT_ACCESS_LOG "./logs/access"
+#define DEFAULT_ERROR_LOG  "./logs/error"
+
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[0;30m"
+#define RED     "\033[0;31m"
+#define GREEN   "\033[0;32m"
+#define YELLOW  "\033[0;33m"
+#define BLUE    "\033[0;34m"
+#define MAGENTA "\033[0;35m"
+#define CYAN    "\033[0;36m"
+#define WHITE   "\033[0;37m"
+
+
