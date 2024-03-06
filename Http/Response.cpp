b5d1ea6c76bff87182e86744c945f8d2d418622b
Achraf Bizyane <abizyane@e1r2p1.1337.ma>
2024-03-06 22:04:56 +0100

    _readFile() function [Implemented] && the tmp requestbody file is created only when the request has body [FIXED]

--- Http/Response.cpp
+++ Http/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: abizyane <abizyane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 23:08:48 by abizyane          #+#    #+#             */
-/*   Updated: 2024/03/04 18:30:45 by abizyane         ###   ########.fr       */
+/*   Updated: 2024/03/06 21:51:37 by abizyane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,24 @@ bool    Response::sent(){
 	return _state == DONE;
 }
 
+Response::ResponseException::ResponseException(e_statusCode status): __status(status){
+}
+
+const char* Response::ResponseException::what() const throw(){
+	return _statusMap[__status].c_str();
+}
+
+e_statusCode    Response::ResponseException::getStatus( void ){
+	return __status;
+}
+
 void	Response::_buildResponse(){
 	_response += "HTTP/1.1 ";
 	_response += _statusMap[_status] + "\r\n";
 	{ // the insertion of headers should be done in the _processResponse functions
 		_headers["Server"] = "Nginx++/1.0.0 (Unix)";
-		_headers["Content-Type"] = _mimeMap["html"]; // the url instead of "html"
-		_headers["Content-Length"] = "45"; // the size of the response body instead of "45"
+		// _headers["Content-Type"] = _mimeMap["html"]; // the url instead of "html"
+		// _headers["Content-Length"] = "45"; // the size of the response body instead of "45"
 		_headers["Date"] = "Mon, 04 Mar 2024 18:21:13 GMT"; // the current date instead of "Mon, 04 Mar 2024 18:21:13 GMT"
 		_headers["Accept-Ranges"] = "bytes";
 		_headers["Content-Language"] = "en"; // the language of the file instead of "en"
@@ -50,17 +61,58 @@ void	Response::_buildResponse(){
 		_response += it->first + ": " + it->second + "\r\n";
 	_response += "\r\n";
 
-	
-	_response += "<html><body><h1>It works!</h1></body></html>"; // the body of the response instead of this
+}
+
+void	Response::_readFile(){ // i used "Makefile" as a test file until the getServerbyhostorport() function is implemented
+	struct stat fileStat;
+	_file.open("Makefile", std::ios::in | std::ios::binary | std::ios::out);
+	stat("Makefile", &fileStat);
+	if (!(fileStat.st_mode & S_IRWXU)) // checks the permissions of the file
+		throw Response::ResponseException(HTTP_FORBIDDEN);
+	if (_file.is_open()){
+		std::stringstream ss;
+		ss << fileStat.st_size;
+		_headers["Content-Length"] = ' ' + ss.str();
+		if (_request->getUri().find_last_of('.') != std::string::npos)
+			_headers["Content-Type"] = _mimeMap[_request->getUri().substr(_request->getUri().find_last_of('.') + 1)];
+		else
+			_headers["Content-Type"] = "text/plain";
+	}
+	else
+		throw Response::ResponseException(HTTP_NOT_FOUND);
 }
 
 void	Response::_processGetResponse(){
+	try {
+		_readFile();
+		
+	}
+	catch (Response::ResponseException &e){
+		_status = e.getStatus();
+		_parse->setParseState(Error);
+	}
 }
 
 void	Response::_processPostResponse(){
+	try {
+		_readFile();
+		
+	}
+	catch (Response::ResponseException &e){
+		_status = e.getStatus();
+		_parse->setParseState(Error);
+	}
 }
 
 void	Response::_processDeleteResponse(){
+	try {
+		_readFile();
+		
+	}
+	catch (Response::ResponseException &e){
+		_status = e.getStatus();
+		_parse->setParseState(Error);
+	}
 }
 
 std::string    Response::GetResponse(size_t lastSent){
@@ -74,10 +126,12 @@ std::string    Response::GetResponse(size_t lastSent){
 			_state = BODY;
 			break;
 		case BODY:
-			index = 45; // 45 is just for testing => [ strtoll(_headers["Content-Length"].c_str(), NULL, 10) - 1 ]
+			index = strtoll(_headers["Content-Length"].c_str(), NULL, 10) - 1;
 			_response.erase(0, lastSent);
 			if (_response.size() > 0 && _bodyIndex < index)
 				response = _response.substr(0, index - _bodyIndex);
+			else if (_response.size() == 0 && _file.peek() != std::ifstream::traits_type::eof())
+				std::getline(_file, _response, '\0');
 			else
 				_state = DONE; 
 			_bodyIndex += lastSent;
